6331b3fedff19cac3fcf4b4119c027d0b0fdcb76
Guillem Parrado <guillempado@gmail.com>
2023-06-12 21:01:31 +0200

    Ex1.2

---
+++ Ex1.2/src/main/java/cat/barcelonactiva/itacademy/ex1_2/Controllers/HelloWorldController.java
@@ -0,0 +1,25 @@
+package cat.barcelonactiva.itacademy.ex1_2.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/HelloWorld")
+    public String saluda(@RequestParam(required = false) String nom) {
+        if (nom == null)
+            nom = "UNKNOWN";
+        return "Hola, " + nom + ". Estàs executant un projecte Maven.";
+    }
+
+    @GetMapping({"/HelloWorld2", "/HelloWorld2/{nom}"})
+    public String saluda2(@PathVariable(required = false, value = "nom") String nom) {
+        if (nom == null)
+            nom = "UNKNOWN";
+        return "Hola, " + nom + ". Estàs executant un projecte Maven.";
+    }
+}
